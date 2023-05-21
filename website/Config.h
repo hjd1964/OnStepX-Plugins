587d3137100d9bb17a5a62a455636015565d280a
Howard Dutton <hjd1964@gmail.com>
2023-05-21 09:23:01 -0400

    Added ability to set Encoder Bridge origins

--- website/Config.h
+++ website/Config.h
@@ -16,7 +16,7 @@
 #define DISPLAY_INTERNAL_TEMPERATURE  OFF //    OFF, ON internal MCU temp. in locale default units.                           Option
 #define DISPLAY_WIFI_SIGNAL_STRENGTH   ON //     ON, Wireless signal strength reported via web interface. OFF otherwise.      Option
 #define DISPLAY_SERVO_MONITOR         OFF //    OFF, ON to display the servo monitor (any axis.)                              Option
-#define DISPLAY_SERVO_EB_CONTROLS     OFF //    OFF, ON to display control to set the servo encoder bridge origin.            Option
+#define DISPLAY_SERVO_ORIGIN_CONTROLS OFF //    OFF, ON to display control to set the absolute encoder origin for servos.     Option
 #define DISPLAY_RESET_CONTROLS         ON //     ON, ON to allow reset of OnStep, FWU for STM32 firmware upload pin HIGH.     Option
 
 #define DISPLAY_SPECIAL_CHARS          ON //     ON, For standard ASCII special symbols (compatibility.)                      Infreq
